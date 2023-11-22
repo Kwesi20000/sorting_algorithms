fd550480d5d3d69dee0d6ef0b6aa9dd0b5abe909
Kwesi20000 <hendrickskumi@yahoo.com>
2023-11-21 21:20:41 -0800

    hendy

--- sort.h
+++ sort.h
@@ -45,12 +45,6 @@ void selection_sort(int *array, size_t size);
 void quick_sort(int *array, size_t size);
 void shell_sort(int *array, size_t size);
 void cocktail_sort_list(listint_t **list);
-void counting_sort(int *array, size_t size);
-void merge_sort(int *array, size_t size);
-void heap_sort(int *array, size_t size);
-void radix_sort(int *array, size_t size);
-void bitonic_sort(int *array, size_t size);
-void quick_sort_hoare(int *array, size_t size);
 
 /* Other Prototypes */
 void swap_int(int *a, int *b);
